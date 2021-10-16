dc1fbccb39ce9cce7e900e9e7a528819a11752ab
number571 <number571@email.com>
2021-10-16 11:10:42 -0400

    refactoring

--- cvm.c
+++ cvm.c
@@ -4,15 +4,19 @@
 
 #include "cvmkernel.h"
 
+#define CVM_RUN     "run"
+#define CVM_BUILD   "build"
+#define CVM_OUTFILE "main.vme"
+
 enum {
-    ERR_NONE,
-    ERR_ARGLEN,
-    ERR_COMMAND,
-    ERR_INOPEN,
-    ERR_OUTOPEN,
-    ERR_COMPILE,
-    ERR_MEMSIZ,
-    ERR_RUN,
+    ERR_NONE    = 0x00,
+    ERR_ARGLEN  = 0x01,
+    ERR_COMMAND = 0x02,
+    ERR_INOPEN  = 0x03,
+    ERR_OUTOPEN = 0x04,
+    ERR_COMPILE = 0x05,
+    ERR_MEMSIZ  = 0x06,
+    ERR_RUN     = 0x07,
 };
 
 static const char *errors[] = {
@@ -29,104 +33,138 @@ static const char *errors[] = {
 static int file_compile(const char *outputf, const char *inputf);
 static int file_run(const char *filename, int **output, int *input);
 
-static void print_array(int *array, int size);
+static void print_json_failed(int retcode);
+static void print_json_success(int *array, int size);
 
 int main(int argc, char const *argv[]) {
+    const char *outfile;
+
     int input[argc];
     int *output;
     int retcode;
 
-    retcode = ERR_NONE;
+    retcode = ERR_COMMAND;
+    outfile = CVM_OUTFILE;
+
     if (argc < 3) {
-        retcode = ERR_ARGLEN;
-        goto close;
-    } 
+        return ERR_ARGLEN;
+    }
 
-    if (strcmp(argv[1], "build") == 0) {
+    if (strcmp(argv[1], CVM_BUILD) == 0) {
         if (argc >= 5 && strcmp(argv[3], "-o") == 0) {
-            retcode = file_compile(argv[4], argv[2]);
-        } else {
-            retcode = file_compile("main.vme", argv[2]);
+            outfile = argv[4];
         }
-        goto close;
-    } 
+        retcode = file_compile(outfile, argv[2]);
+    }
 
-    if (strcmp(argv[1], "run") == 0) {
+    if (strcmp(argv[1], CVM_RUN) == 0) {
         input[0] = argc-3;
         for (int i = 0; i < argc-3; ++i) {
             input[i+1] = atoi(argv[i+3]);
         }
+
     	retcode = file_run(argv[2], &output, input);
-    	if (retcode != ERR_NONE) {
-            goto close;
-    	}
-        print_array(output+1, output[0]);
-        free(output);
-        goto close;
-    } 
-
-    retcode = ERR_COMMAND;
-
-close:
-    if (retcode != ERR_NONE) {
-        fprintf(stderr, "error: %s\n", errors[retcode]);
+    	if (retcode == ERR_NONE) {
+            print_json_success(output+1, output[0]);
+            free(output);
+    	} else {
+            print_json_failed(retcode);
+        }
     }
+
     return retcode;
 }
 
 static int file_compile(const char *outputf, const char *inputf) {
     FILE *output, *input;
     int retcode;
+
     input = fopen(inputf, "r");
     if (input == NULL) {
         return ERR_INOPEN;
     }
+
     output = fopen(outputf, "wb");
-    if (input == NULL) {
+    if (output == NULL) {
+        fclose(input);
         return ERR_OUTOPEN;
     }
+
     retcode = cvm_compile(output, input);
+
     fclose(input);
     fclose(output);
+    
     if (retcode != ERR_NONE) {
         return ERR_COMPILE;
     }
+    
     return ERR_NONE;
 }
 
-static int file_run(const char *filename, int **output, int *input) {
+static int file_run(const char *inputf, int **output, int *input) {
     unsigned char *memory;
-    FILE *reader;
     int fsize, retcode;
-    reader = fopen(filename, "rb");
+    FILE *reader;
+
+    reader = fopen(inputf, "rb");
     if (reader == NULL) {
         return ERR_INOPEN;
     }
-    // read len of code
+
+    // read size of code
     fseek(reader, 0, SEEK_END);
     fsize = ftell(reader);
     fseek(reader, 0, SEEK_SET);
+    
     // insert code into memory
     memory = (unsigned char*)malloc(sizeof(char)*fsize);
     fread(memory, fsize, sizeof(char), reader);
     fclose(reader);
+    
     fsize = cvm_load(memory, fsize);
     free(memory);
     if (fsize < 0) {
         return ERR_MEMSIZ;
     }
+    
     // run code in memory
     retcode = cvm_run(output, input);
     if (retcode != ERR_NONE) {
         return ERR_RUN;
     }
+    
     return ERR_NONE;
 }
 
-static void print_array(int *array, int size) {
-    printf("[ ");
+static void print_json_failed(int retcode) {
+    // begin object
+    printf("{\n");
+
+    // error:string
+    printf("\t\"error\": \"%s\",\n", errors[retcode]);
+
+    // return:int
+    printf("\t\"return\": %d\n", retcode);
+
+    // end object
+    printf("}\n");
+}
+
+static void print_json_success(int *array, int size) {
+    // begin object
+    printf("{\n");
+
+    // result:array
+    printf("\t\"result\": [");
     for (int i = 0; i < size; ++i) {
-        printf("%d ", array[i]);
+        printf("%d%c", array[i], (i == size-1) ? '\0' : ',');
     }
-    printf("]\n");
+    printf("],\n");
+
+    // return:int
+    printf("\t\"return\": 0\n");
+
+    // end object
+    printf("}\n");
 }
--- cvmkernel.c
+++ cvmkernel.c
@@ -9,39 +9,33 @@
 #include "extclib/type/hashtab.h"
 #include "extclib/type/stack.h"
 
-// RESULT = INSTR.CODE | RETURN.CODE
-#define WRAP(x, y) ((x) << 8 | (y))
-
 // Comment this line if you are
 // need use only main inctructions.
-#define ADDITIONAL_INSTRUCTION
+#define CVM_KERNEL_IAPPEND
 
-// Num of all instructions.
-#ifdef ADDITIONAL_INSTRUCTION
-	#define INSTR_SIZE 28
+// Number of all instructions.
+#ifdef CVM_KERNEL_IAPPEND
+	#define CVM_KERNEL_ISIZE 29
 #else
-	#define INSTR_SIZE 14
+	#define CVM_KERNEL_ISIZE 15
 #endif
 
-// 4096 BYTE
-#define MEMRY_SIZE (4 << 10)
-
-// 1024 INT32
-#define STACK_SIZE (1 << 10)
-
-// 1024 LIST
-#define HSTAB_SIZE (1 << 10)
+// Memory settings.
+#define CVM_KERNEL_SMEMORY (1 << 10) // 1024 INT32
+#define CVM_KERNEL_HMEMORY (1 << 10) // 1024 LIST
+#define CVM_KERNEL_CMEMORY (4 << 10) // 4096 BYTE
 
 // N - number
 // C - char
 enum {
 	// 0xNN 
-	// PSEUDO INSTRUCTIONS (3)
-	C_PASS = 0x00, // 0 bytes
-	C_CMNT = 0x11, // 0 bytes
-	C_LABL = 0x22, // 0 bytes
+	// PSEUDO INSTRUCTIONS (4)
+	C_UNDF = 0x00, // 0 bytes
+	C_VOID = 0x11, // 0 bytes
+	C_CMNT = 0x22, // 0 bytes
+	C_LABL = 0x33, // 0 bytes
 	// 0xNC 
-	// MAIN INSTRUCTIONS (12)
+	// MAIN INSTRUCTIONS (11)
 	C_PUSH = 0x0A, // 5 bytes
 	C_POP  = 0x0B, // 1 bytes
 	C_ADD  = 0x0C, // 1 bytes
@@ -53,7 +47,7 @@ enum {
 	C_LOAD = 0x1C, // 1 bytes
 	C_CALL = 0x1D, // 1 bytes
 	C_HLT  = 0x1E, // 1 bytes
-#ifdef ADDITIONAL_INSTRUCTION
+#ifdef CVM_KERNEL_IAPPEND
 	// 0xCN 
 	// ADD INSTRUCTIONS (14)
 	C_MUL  = 0xA0, // 1 bytes
@@ -77,16 +71,17 @@ enum {
 
 static struct virtual_machine {
 	int32_t mmused;
-	uint8_t memory[MEMRY_SIZE];
+	uint8_t memory[CVM_KERNEL_CMEMORY];
 	struct {
 		int  bcode;
 		char *mnem;
-	} bclist[INSTR_SIZE];
+	} bclist[CVM_KERNEL_ISIZE];
 } VM = {
 	.mmused = 0,
 	.bclist = {
 		// PSEUDO INSTRUCTIONS
-		{ C_PASS,  ""    }, // 0 arg
+		{ C_UNDF, ""     }, // 0 arg
+		{ C_VOID, ""     }, // 0 arg
 		{ C_CMNT, ";"    }, // 0 arg
 		{ C_LABL, "labl" }, // 1 arg
 		// MAIN INSTRUCTIONS
@@ -101,7 +96,7 @@ static struct virtual_machine {
 		{ C_LOAD, "load" }, // 0 arg, 1 stack
 		{ C_CALL, "call" }, // 0 arg, 1 stack
 		{ C_HLT,  "hlt"  }, // 0 arg, 0 stack
-#ifdef ADDITIONAL_INSTRUCTION
+#ifdef CVM_KERNEL_IAPPEND
 		// ADD INSTRUCTIONS
 		{ C_MUL,  "mul"  }, // 0 arg, 2 stack
 		{ C_DIV,  "div"  }, // 0 arg, 2 stack
@@ -121,21 +116,24 @@ static struct virtual_machine {
 	},
 };
 
-static char *readcode(char *line, uint8_t *opcode);
+static char *read_code(char *line, uint8_t *opcode);
+static uint16_t wrap_return(uint8_t x, uint8_t y);
 
 static uint32_t join_8bits_to_32bits(uint8_t *bytes);
 static void split_32bits_to_8bits(uint32_t num, uint8_t *bytes);
 
-static char *strtolower(char *str);
-static int strnull(char *str);
+static char *str_trim_spaces(char *str);
+static char *str_set_end(char *str);
 
-// pegjs
+static char *str_to_lower(char *str);
+static int str_is_void(char *str);
+
 extern int cvm_run(int32_t **output, int32_t *input) {
 	stack_t *stack;
 	uint8_t opcode;
 	int32_t mi;
 
-	stack = stack_new(STACK_SIZE, sizeof(int32_t));
+	stack = stack_new(CVM_KERNEL_SMEMORY, sizeof(int32_t));
 	for (int i = 0; i < input[0]; ++i) {
 		stack_push(stack, &input[i+1]);
 	}
@@ -147,8 +145,8 @@ extern int cvm_run(int32_t **output, int32_t *input) {
 			case C_PUSH: {
 				int32_t num;
 				uint8_t bytes[4];
-				if (stack_size(stack) == STACK_SIZE) {
-					return WRAP(opcode, 1);
+				if (stack_size(stack) == CVM_KERNEL_SMEMORY) {
+					return wrap_return(opcode, 1);
 				}
 				memcpy(bytes, VM.memory + mi, 4); mi += 4;
 				num = (int32_t)join_8bits_to_32bits(bytes);
@@ -157,12 +155,12 @@ extern int cvm_run(int32_t **output, int32_t *input) {
 			break;
 			case C_POP: {
 				if (stack_size(stack) == 0) {
-					return WRAP(opcode, 1);
+					return wrap_return(opcode, 1);
 				}
 				stack_pop(stack);
 			}
 			break;
-		#ifdef ADDITIONAL_INSTRUCTION
+		#ifdef CVM_KERNEL_IAPPEND
 			case C_NOT: {
 				int32_t x;
 				x = ~*(int32_t*)stack_pop(stack);
@@ -170,7 +168,7 @@ extern int cvm_run(int32_t **output, int32_t *input) {
 			}
 			break;
 		#endif
-		#ifdef ADDITIONAL_INSTRUCTION
+		#ifdef CVM_KERNEL_IAPPEND
 			case C_MUL: case C_DIV:
 			case C_MOD: case C_AND: 
 			case C_OR:  case C_XOR:
@@ -179,12 +177,12 @@ extern int cvm_run(int32_t **output, int32_t *input) {
 			case C_ADD: case C_SUB: {
 				int32_t x, y;
 				if (stack_size(stack) < 2) {
-					return WRAP(opcode, 1);
+					return wrap_return(opcode, 1);
 				}
 				x = *(int32_t*)stack_pop(stack);
 				y = *(int32_t*)stack_pop(stack);
 				switch(opcode) {
-				#ifdef ADDITIONAL_INSTRUCTION
+				#ifdef CVM_KERNEL_IAPPEND
 					case C_MUL:
 						y *= x;
 					break;
@@ -224,28 +222,28 @@ extern int cvm_run(int32_t **output, int32_t *input) {
 			case C_STOR: {
 				int32_t num1, num2;
 				if (stack_size(stack) < 2) {
-					return WRAP(opcode, 1);
+					return wrap_return(opcode, 1);
 				}
 				num1 = *(int32_t*)stack_pop(stack);
 				num2 = *(int32_t*)stack_pop(stack);
 				if (num1 < 0) {
 					num1 = stack_size(stack) + num1;
 					if (num1 < 0) {
-						return WRAP(opcode, 2);
+						return wrap_return(opcode, 2);
 					}
 				} else {
 					if (num1 >= stack_size(stack)) {
-						return WRAP(opcode, 3);
+						return wrap_return(opcode, 3);
 					}
 				}
 				if (num2 < 0) {
 					num2 = stack_size(stack) + num2;
 					if (num2 < 0) {
-						return WRAP(opcode, 4);
+						return wrap_return(opcode, 4);
 					}
 				} else {
 					if (num2 >= stack_size(stack)) {
-						return WRAP(opcode, 5);
+						return wrap_return(opcode, 5);
 					}
 				}
 				num2 = *(int32_t*)stack_get(stack, num2);
@@ -255,67 +253,67 @@ extern int cvm_run(int32_t **output, int32_t *input) {
 			case C_LOAD: {
 				int32_t num;
 				if (stack_size(stack) == 0) {
-					return WRAP(opcode, 1);
+					return wrap_return(opcode, 1);
 				}
 				num = *(int32_t*)stack_pop(stack);
 				if (num < 0) {
 					num = stack_size(stack) + num;
 					if (num < 0) {
-						return WRAP(opcode, 2);
+						return wrap_return(opcode, 2);
 					}
 				} else {
 					if (num >= stack_size(stack)) {
-						return WRAP(opcode, 3);
+						return wrap_return(opcode, 3);
 					}
 				}
 				num = *(int32_t*)stack_get(stack, num);
 				stack_push(stack, &num);
 			}
 			break;
-		#ifdef ADDITIONAL_INSTRUCTION
+		#ifdef CVM_KERNEL_IAPPEND
 			case C_JMP: {
 				int32_t num;
 				if (stack_size(stack) == 0) {
-					return WRAP(opcode, 1);
+					return wrap_return(opcode, 1);
 				}
 				num = *(int32_t*)stack_pop(stack);
 				if (num < 0) {
 					num = stack_size(stack) + num;
 					if (num < 0) {
-						return WRAP(opcode, 2);
+						return wrap_return(opcode, 2);
 					}
 					num = *(int32_t*)stack_get(stack, num);
 				}
 				if (num < 0 || num >= VM.mmused) {
-					return WRAP(opcode, 3);
+					return wrap_return(opcode, 3);
 				}
 				mi = num;
 			}
 			break;
 		#endif
-		#ifdef ADDITIONAL_INSTRUCTION
+		#ifdef CVM_KERNEL_IAPPEND
 			case C_JGE: case C_JLE: case C_JNE: 
 		#endif 
 			case C_JL: case C_JG: case C_JE: {
 				int32_t num, x, y;
 				if (stack_size(stack) < 3) {
-					return WRAP(opcode, 1);
+					return wrap_return(opcode, 1);
 				}
 				num = *(int32_t*)stack_pop(stack);
 				if (num < 0) {
 					num = stack_size(stack) + num;
 					if (num < 0) {
-						return WRAP(opcode, 2);
+						return wrap_return(opcode, 2);
 					}
 					num = *(int32_t*)stack_get(stack, num);
 				}
 				if (num < 0 || num >= VM.mmused) {
-					return WRAP(opcode, 3);
+					return wrap_return(opcode, 3);
 				}
 				x = *(int32_t*)stack_pop(stack);
 				y = *(int32_t*)stack_pop(stack);
 				switch(opcode) {
-				#ifdef ADDITIONAL_INSTRUCTION
+				#ifdef CVM_KERNEL_IAPPEND
 					case C_JNE:
 						if (y != x) {
 							mi = num;
@@ -351,18 +349,18 @@ extern int cvm_run(int32_t **output, int32_t *input) {
 				}
 			}
 			break;
-		#ifdef ADDITIONAL_INSTRUCTION
+		#ifdef CVM_KERNEL_IAPPEND
 			case C_ALLC: {
 				int32_t num, null;
 				if (stack_size(stack) == 0) {
-					return WRAP(opcode, 1);
+					return wrap_return(opcode, 1);
 				}
 				num = *(int32_t*)stack_pop(stack);
 				if (num < 0) {
-					return WRAP(opcode, 2);
+					return wrap_return(opcode, 2);
 				}
-				if (stack_size(stack)+num >= STACK_SIZE) {
-					return WRAP(opcode, 3);
+				if (stack_size(stack)+num >= CVM_KERNEL_SMEMORY) {
+					return wrap_return(opcode, 3);
 				}
 				null = 0;
 				for (int i = 0; i < num; ++i) {
@@ -374,18 +372,18 @@ extern int cvm_run(int32_t **output, int32_t *input) {
 			case C_CALL: {
 				int32_t num;
 				if (stack_size(stack) == 0) {
-					return WRAP(opcode, 1);
+					return wrap_return(opcode, 1);
 				}
 				num = *(int32_t*)stack_pop(stack);
 				if (num < 0) {
 					num = stack_size(stack) + num;
 					if (num < 0) {
-						return WRAP(opcode, 2);
+						return wrap_return(opcode, 2);
 					}
 					num = *(int32_t*)stack_get(stack, num);
 				}
 				if (num < 0 || num >= VM.mmused) {
-					return WRAP(opcode, 3);
+					return wrap_return(opcode, 3);
 				}
 				stack_push(stack, &mi);
 				mi = num;
@@ -407,11 +405,11 @@ close:
 		(*output)[i+1] = *(int32_t*)stack_pop(stack);
 	}
 	stack_free(stack);
-	return WRAP(0x00, 0);
+	return wrap_return(0x00, 0);
 }
 
 extern int cvm_load(uint8_t *memory, int32_t msize) {
-	if (msize < 0 || msize >= MEMRY_SIZE) {
+	if (msize < 0 || msize >= CVM_KERNEL_CMEMORY) {
 		return 1;
 	}
 	memcpy(VM.memory, memory, msize);
@@ -421,26 +419,22 @@ extern int cvm_load(uint8_t *memory, int32_t msize) {
 
 extern int cvm_compile(FILE *output, FILE *input) {
 	hashtab_t *hashtab;
-	int line_index, byte_index, err_exist;
+	int byte_index;
 	char buffer[BUFSIZ];
 	char *arg;
 	uint8_t opcode;
 
-	hashtab = hashtab_new(HSTAB_SIZE);
-	line_index = 0;
+	hashtab = hashtab_new(CVM_KERNEL_HMEMORY);
 	byte_index = 0;
-	err_exist  = 0;
 
 	while(fgets(buffer, BUFSIZ, input) != NULL) {
-		++line_index;
-		arg = readcode(buffer, &opcode);
-		if(opcode == C_CMNT || strnull(arg)) {
+		arg = read_code(buffer, &opcode);
+		if(opcode == C_CMNT || opcode == C_VOID) {
 			continue;
 		}
-		if (opcode == C_PASS) {
-			err_exist = 1;
-			fprintf(stderr, "syntax error: line %d\n", line_index);
-			continue;
+		if (opcode == C_UNDF) {
+			hashtab_free(hashtab);
+			return 1;
 		}
 		switch(opcode) {
 			case C_LABL: {
@@ -457,17 +451,12 @@ extern int cvm_compile(FILE *output, FILE *input) {
 		}
 	}
 
-	if (err_exist) {
-		hashtab_free(hashtab);
-		return 1;
-	}
-
 	fseek(input, 0, SEEK_SET);
 
 	while(fgets(buffer, BUFSIZ, input) != NULL) {
-		arg = readcode(buffer, &opcode);
+		arg = read_code(buffer, &opcode);
 		switch (opcode) {
-			case C_PASS: case C_CMNT: case C_LABL: {
+			case C_VOID: case C_CMNT: case C_LABL: {
 			}
 			break;
 			case C_PUSH: {
@@ -494,43 +483,42 @@ extern int cvm_compile(FILE *output, FILE *input) {
 	return 0;
 }
 
-static char *readcode(char *line, uint8_t *opcode) {
+// read opcode from string and return
+// pointer to first argument if exists.
+static char *read_code(char *line, uint8_t *opcode) {
 	char *ptr;
-	// pass spaces
-	ptr = line;
-	while(isspace(*ptr)) {
-		++ptr;
-	}
-	// read chars of opcode
-	line = ptr;
-	while(!isspace(*ptr)) {
-		++ptr;
-	}
-	*ptr = '\0';
-	// get opcode int
-	*opcode = C_PASS;
-	for (int i = 0; i < INSTR_SIZE; ++i) {
-		if (strcmp(strtolower(line), VM.bclist[i].mnem) == 0) {
+
+	line = str_trim_spaces(line);
+	ptr = str_set_end(line);
+
+	*opcode = C_UNDF;
+	for (int i = 0; i < CVM_KERNEL_ISIZE; ++i) {
+		if (strcmp(str_to_lower(line), VM.bclist[i].mnem) == 0) {
 			*opcode = VM.bclist[i].bcode;
 			break;
 		}
 	}
-	// pass if opcode without arguments 
-	if (*opcode != C_PUSH && *opcode != C_LABL) {
-		return line;
+
+	if (str_is_void(line)) {
+		*opcode = C_VOID;
 	}
-	// pass spaces
-	++ptr;
-	while(isspace(*ptr)) {
-		++ptr;
+
+	switch(*opcode) {
+		case C_PUSH: case C_LABL:
+			break;
+		default:
+			return NULL;
 	}
-	// read chars of first argument
-	line = ptr;
-	while(!isspace(*ptr)) {
-		++ptr;
-	}
-	*ptr = '\0';
+
+	line = str_trim_spaces(++ptr);
+	str_set_end(line);
+
+	// pointer to first arg
 	return line;
+}
+
+static uint16_t wrap_return(uint8_t x, uint8_t y) {
+	return ((uint16_t)x << 8) | y;
 }
 
 static uint32_t join_8bits_to_32bits(uint8_t *bytes) {
@@ -547,7 +535,23 @@ static void split_32bits_to_8bits(uint32_t num, uint8_t *bytes) {
 	}
 }
 
-static char *strtolower(char *str) {
+static char *str_set_end(char *str) {
+	char *ptr = str;
+	while(!isspace(*ptr)) {
+		++ptr;
+	}
+	*ptr = '\0';
+	return ptr;
+}
+
+static char *str_trim_spaces(char *str) {
+	while(isspace(*str)) {
+		++str;
+	}
+	return str;
+}
+
+static char *str_to_lower(char *str) {
 	int len = strlen(str);
 	for (int i = 0; i < len; ++i) {
 		str[i] = tolower(str[i]);
@@ -555,12 +559,9 @@ static char *strtolower(char *str) {
 	return str;
 }
 
-static int strnull(char *str) {
+static int str_is_void(char *str) {
 	while(isspace(*str)) {
 		++str;
 	}
-	if(*str == '\0') {
-		return 1;
-	}
-	return 0;
+	return *str == '\0';
 }
